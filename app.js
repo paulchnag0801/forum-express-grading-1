c53a0d07bc1cb2644ebe2c8353dbbc7488ca79f2
paulchnag0801 <x1232178987@gmail.com>
2021-11-10 00:50:02 +0800

    fix port

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const session = require('express-session')
 const passport = require('./config/passport')
 const methodOverride = require('method-override')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // 設定 view engine 使用 handlebars
 app.engine('handlebars', handlebars())
