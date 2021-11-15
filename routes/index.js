2a619b27eee46ca221059a2216adf1936eec217b
paulchnag0801 <x1232178987@gmail.com>
2021-11-15 17:03:14 +0800

    Add post comment feature on restaurant page

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const restController = require('../controllers/restController.js')
 const adminController = require('../controllers/adminController.js')
 const userController = require('../controllers/userController.js')
 const categoryController = require('../controllers/categoryController.js')
+const commentController = require('../controllers/commentController.js')
 
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
@@ -34,9 +35,12 @@ module.exports = (app, passport) => {
 
   //在 /restaurants 底下則交給 restController.getRestaurants 來處理
   app.get('/restaurants', authenticated, restController.getRestaurants)
-  
+
   //顯示單一餐廳路由
   app.get('/restaurants/:id', authenticated, restController.getRestaurant)
+
+  //新增評論路由
+  app.post('/comments', authenticated, commentController.postComment)
 
   // 連到 /admin 頁面就轉到 /admin/restaurants
   app.get('/admin', authenticatedAdmin, (req, res) =>
