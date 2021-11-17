175497cc09dde0c493818a8a58459fcc915f2226
paulchnag0801 <x1232178987@gmail.com>
2021-11-17 22:20:46 +0800

    Create favorite model and set association between restaurant and user

--- models/restaurant.js
+++ models/restaurant.js
@@ -4,8 +4,15 @@ module.exports = (sequelize, DataTypes) => {
   class Restaurant extends Model {
     static associate(models) {
       // define association here
-      Restaurant.belongsTo(models.Category) // 加入關聯設定
+      Restaurant.belongsTo(models.Category) 
       Restaurant.hasMany(models.Comment)
+      //加入多對多的關聯語法，從餐廳出發，去尋找一間餐廳會被哪些使用者收藏
+      Restaurant.belongsToMany(models.User, {
+        //belongsToMany 表示這是一個多對多關係
+        through: models.Favorite, //透過through告訴Sequelize，應該從Favorite的models去找
+        foreignKey: 'RestaurantId', //找出餐廳的id
+        as: 'FavoritedUsers', //新增一個別名，將找到的user存進去。就可以用 restaurant.FavoritedUsers 來呼叫出和餐廳相關的 User 物件。
+      })
     }
   }
   Restaurant.init(
--- models/user.js
+++ models/user.js
@@ -10,6 +10,12 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.hasMany(models.Comment)
+      User.belongsToMany(models.Restaurant, {
+        //一個使用者同時可以收藏很多餐廳，所以從使用者出發去尋找這個使用者多少間餐廳。
+        through: models.Favorite, //透過Favorite的models
+        foreignKey: 'UserId', //找到使用者的id
+        as: 'FavoritedRestaurants', //新增一個別名，將找到的餐廳放進去，同樣的透過類似 user.FavoritedRestaurants 這樣的方法來呼叫出和 user 實例有收藏關係的所有餐廳。
+      })
     }
   }
   User.init(
