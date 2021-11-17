9203a40d22d39ae2be14dce490baa5dd03044a20
paulchnag0801 <x1232178987@gmail.com>
2021-11-18 01:23:06 +0800

    Create followship model and add followers and followings to users

--- models/user.js
+++ models/user.js
@@ -21,6 +21,18 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'UserId',
         as: 'LikedRestaurants',
       })
+      User.belongsToMany(User, {
+        //一個使用者可以被很多使用者追蹤，所以從user自己出發，去尋找有多少人追蹤自己。
+        through: models.Followship, //透過 Followship model 來查資料。
+        foreignKey: 'followingId', //followingId 被追蹤者的id
+        as: 'Followers', //新增一個別名，將找到的追蹤者的id，放進Followers的裡面
+      })
+      User.belongsToMany(User, {
+        //一個使用者可以追蹤很多個使用者，一樣從user自己出發，但使用自己的id去尋找你追蹤了多少人。
+        through: models.Followship, //透過 Followship Model
+        foreignKey: 'followerId', //followerId 追蹤者的id
+        as: 'Followings', //將你追蹤的所有人的id，放進Followings。
+      })
     }
   }
   User.init(
