7031796e0b392343467c0a0cca9e2a86148aff23
paulchnag0801 <x1232178987@gmail.com>
2021-11-24 12:20:47 +0800

    refactor adminController.getRestaurants

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,3 +1,4 @@
+//這裡是view的adminController
 const fs = require('fs')
 const db = require('../models')
 const Restaurant = db.Restaurant
@@ -5,15 +6,12 @@ const User = db.User
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 const Category = db.Category
+const adminService = require('../services/adminService.js')
 
 const adminController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({
-      raw: true,
-      nest: true,
-      include: [Category],
-    }).then((restaurants) => {
-      return res.render('admin/restaurants', { restaurants: restaurants })
+    adminService.getRestaurants(req, res, (data) => {
+      return res.render('admin/restaurants', data)
     })
   },
   createRestaurant: (req, res) => {
