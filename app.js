a0f5819ac06bf07435a2ce1a548f1a464ccd8cda
paulchnag0801 <x1232178987@gmail.com>
2021-11-25 15:55:07 +0800

    add authenticated and authenticatedAdmin

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const db = require('./models')
 const flash = require('connect-flash')
 const helpers = require('./_helpers')
 
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-
-const session = require('express-session')
 const passport = require('./config/passport')
+const session = require('express-session')
 const methodOverride = require('method-override')
 const app = express()
 const port = process.env.PORT || 3000
